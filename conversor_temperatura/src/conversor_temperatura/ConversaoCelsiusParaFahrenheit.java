1b1f08d68cfae8a8519383e7aa8e288a73fd5d50
Bruna Lozinski <lozinskibruna@gmail.com>
2021-03-02 11:23:26 -0300

    Aula 03-01-2021 - Conversor de Temperatura

---
+++ conversor_temperatura/src/conversor_temperatura/ConversaoCelsiusParaFahrenheit.java
@@ -0,0 +1,23 @@
+package conversor_temperatura;
+
+public class ConversaoCelsiusParaFahrenheit {
+
+	public static void main(String[] args) {
+		
+		double celsius;
+		double fahrenheit;
+		
+		celsius = 32.0;
+		
+		fahrenheit = (9 * celsius + 160) / 5;
+		
+		System.out.print(celsius + " equivale a " fahrenheit);
+		
+		
+		
+		
+	}
+	
+	
+	
+}
---
+++ temperatura/src/temperatura/Temperatura.java
@@ -0,0 +1,17 @@
+package temperatura;
+
+public class Temperatura {
+
+	public static void main(String[] args) {
+
+		float celsius;
+		float fahrenheit;
+
+		celsius = 2;
+		fahrenheit =
+
+				System.out.print(temperatura);
+
+	}
+
+}
